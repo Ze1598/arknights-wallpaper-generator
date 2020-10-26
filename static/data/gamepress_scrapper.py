4c63d40bd71fb217d5af94cfbb0ce61815eed9a5
José Fernando Costa <jose.fernando.costa.1998@gmail.com>
2020-10-26 21:26:56 +0000

    Added functionality to the scraper to scrape skins information (exported as a JSON file)

--- static/data/gamepress_scrapper.py
+++ static/data/gamepress_scrapper.py
@@ -1,6 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
 import pickle
+import json
+import datetime
 import pandas as pd
 import logging
 logging.basicConfig(level=logging.INFO)
@@ -9,63 +11,107 @@ logging.basicConfig(level=logging.INFO)
 req = requests.get("https://gamepress.gg/arknights/tools/interactive-operator-list")
 soup = BeautifulSoup(req.content, "lxml")
 
+# Get all the table cells (<td>) with information about the operators
 op_list = soup.find_all("td", class_="operator-cell")
 op_dict = {}
 for op in op_list:
-	name = op.find("div", class_="operator-title").a.text
-	page = "https://gamepress.gg" + op.find("div", class_="operator-title").a["href"]
-	op_dict[name] = page
+    # Get the name and their personal page from these HTML elements
+    name = op.find("div", class_="operator-title").a.text
+    page = "https://gamepress.gg" + op.find("div", class_="operator-title").a["href"]
+    # Add the new information to the dictionary
+    op_dict[name] = page
+    logging.info(f"{datetime.datetime.now()}: Found {name}")
 
+# Write this dictionary to a pickle file
 with open("operator_pages.pickle", "wb") as f:
-	pickle.dump(op_dict, f)
-	logging.info("Created pickle file with operator pages.")
+    pickle.dump(op_dict, f)
+    logging.info(f"{datetime.datetime.now()}: Created pickle file with operator pages")
+
 # -----------------------------------------------------------------------------
 
-# Scrape the operator's gallery URLs and star rankings
+# Load the pickle file with the URL to the operator pages
 with open("operator_pages.pickle", "rb") as f:
-	operator_pages = pickle.load(f)
+    operator_pages = pickle.load(f)
 
-# operator_pages ={
-# 	"Ch'en": "https://gamepress.gg/arknights/operator/chen",
-# 	"Aak": "https://gamepress.gg/arknights/operator/aak",
-# 	"Blaze": "https://gamepress.gg/arknights/operator/blaze"
-# }
+# List to contain lists of individual operator information
 operators_info = list()
+# List to contain dictionaries of operator skins
+skins_info = list()
 for operator in operator_pages:
-	logging.info(f"Scraping {operator}")
-	name = operator
-	page = operator_pages[operator]
-	req = requests.get(page)
-	soup = BeautifulSoup(req.content, "lxml")
+    logging.info(f"{datetime.datetime.now()}: Scraping {operator}")
+    name = operator
+    page = operator_pages[operator]
 
-	# e1_img = soup.find("div", id="image-tab-1")
+    # Get the operator's page
+    req = requests.get(page)
+    soup = BeautifulSoup(req.content, "lxml")
 
-	# e1_img = soup.find("div", class_="operator-image")\
-		# .find("a")["href"]
+    # Find all the elements for operator art
+    images = soup.find_all("div", class_="operator-image")
 
-	images = soup.find_all("div", class_="operator-image")
+    # Find all the elements for promotion levels
+    prom_levels = soup.find("ul", class_="operator-image-tabs")\
+        .find_all("li")
 
-	prom_levels = soup.find("ul", class_="operator-image-tabs")\
-		.find_all("li")
+    # Get the URL for the base/E1 art
+    e1_img = images[0].a["href"]
 
-	e1_img = images[0].a["href"]
+    # If the list of promotion levels has less than 3 levels, then the\
+    # operator doesn't have an E2 promotion
+    if len(prom_levels) < 3:
+        has_e2 = 0
+        e2_img = ""
+    # Otherwise get their E2 art
+    else:
+        has_e2 = 1
+        e2_img = images[2].a["href"]
 
-	# If the list of promotion levels has less than 3 options, then the\
-	# operator doesn't have an E2 promotion
-	if len(prom_levels) < 3:
-		has_e2 = 0
-		e2_img = ""
-	else:
-		has_e2 = 1
-		# url_up_to_prom_level = e1_img.split(".png")[0]
-		# e2_img = url_up_to_prom_level[:-1] + "2.png"
-		e2_img = images[2].a["href"]
+    # Find the operator's rank by counting the number of stars they have
+    num_stars = len(soup.find("div", class_="rarity-cell").find_all("img"))
 
-	num_stars = len(soup.find("div", class_="rarity-cell").find_all("img"))
+    # HTML unordered list (<ul>) of skins
+    skins_ul = soup.find("ul", class_="skin-image-tabs")
+    # Count the number of items in the list above to know the number of skins available
+    num_skins = len(skins_ul.find_all("li", class_="tab-link"))
 
-	operator_info = [name, num_stars, e1_img, e2_img, has_e2]
-	operators_info.append(operator_info)
+    # If the operator has skins, scrape their URL
+    if num_skins > 0:
+        # Since the operator skins are the last `num_skins` <li> items of\
+        # the <ul>, then create a range to get the last `num_skins` skins\
+        # from the `images` <ul>
+        _start = -1
+        _stop = -num_skins-1
+        skins_li = images[_start: _stop: -1]
+        op_skins = {name: []}
+        # Now get a list of all the availble skins' art URLs as a dictionary
+        for i, skin_html in enumerate(skins_li):
+            op_skins[name].append({f"Skin {i+1}": "https://gamepress.gg" + skin_html.img["src"]})
+        # Add the new info to the running list
+        skins_info.append(op_skins)
 
+        # Reset this list so skins' information does not carry over for the\
+        # next operator
+        skins_ul = list()
+        # Ditto for this list
+        op_skins = list()
+
+    # Add this operator's info to the running list
+    operator_info = [name, num_stars, e1_img, e2_img, has_e2]
+    operators_info.append(operator_info)
+
+
+# Create a DF for the list of lists of operators
 info_df = pd.DataFrame(operators_info)
 info_df.columns = ["name", "num_stars", "e1_img", "e2_img", "has_e2"]
-info_df.to_csv("operators_info.csv", index=False)
+# Load the CSV of operator colors
+colors_csv = pd.read_csv("colors.csv")
+# Join the two DFs
+csv_info_join = info_df.join(colors_csv, rsuffix="__", how="inner")
+# Remove the duplicated column of operator names
+csv_info_join.drop(["name__"], axis="columns", inplace=True)
+# Export the complete DF as a CSV
+csv_info_join.to_csv("operators_info.csv", index=False)
+
+# Export the list of skins dictionaries as a JSON
+with open("skins_info.json", "w") as f:
+    json.dump(skins_info, f, indent=2)
