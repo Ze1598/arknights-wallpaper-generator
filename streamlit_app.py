d1f706ca8dcf8a572c55c78dbab7a62959667c05
José Costa <jose.fernando.costa.1998@gmail.com>
2021-05-13 20:58:13 +0100

    Reverted to Streamlit 0.69.2 to use beta_color_picker

--- streamlit_app.py
+++ streamlit_app.py
@@ -118,8 +118,9 @@ if custom_bg_img != None:
     pil_custom_bg_img = Image.open(custom_bg_img).resize((640, 1280)).save(custom_bg_path)
 
 # Change the operator theme color
-custom_op_color = st.color_picker(
-    "Feel free to change the operator theme color", op_default_color)
+# Using the beta version until the generally available version is fixed in Streamlit 
+#custom_op_color = st.color_picker("Feel free to change the operator theme color", op_default_color)
+custom_op_color = st.beta_color_picker("Feel free to change the operator theme color", op_default_color)
 
 # Put together relevant operator information in a single dictionary
 operator_info = {
