ed6764e6b3620a111090e7316e155c2d5dfa3fcb
Ze1598 <jose.fernando.costa.1998@gmail.com>
2024-12-07 13:17:20 +0000

    Updated output dataset location and stored dataset as an array instead of object

--- static/prts_scrape.py
+++ static/prts_scrape.py
@@ -5,20 +5,24 @@ import time
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from datetime import datetime
 from random import randint
+import os
 
 if __name__ == "__main__":
+    # Base path to write to
+    out_path = os.path.join(os.getcwd(), "static", "data")
+
+    # Initialize class to scrape list of existing operators
     scrapper = PrtsScrapper()
     num_operators = scrapper.num_operators_available
     operator_pages = scrapper.operators_url_dict
+
     
-    with open('operators_list.json', 'w') as f:
+    with open(os.path.join(out_path, "operators_list.json"), "w") as f:
         json.dump(operator_pages, f, indent=4, ensure_ascii=False)
 
 
-    # Keep track of operator details in a dict
-    operators_dict = dict()
-    # Capture scrape errors to pick up later
-    fail_dict = dict()
+    # Keep track of operator details in a list of dictionaries
+    operators_list = list()
 
     counter = 1
 
@@ -41,27 +45,23 @@ if __name__ == "__main__":
             translated_name = operator.name_translated
 
             # Entry for this operator
-            operators_dict[translated_name] = operator.operator_details
+            operators_list.append(operator.operator_details)
 
         # Use the except to retry scrape
         except:
             time.sleep(60)
-            # print(f"RETRY {counter} / {num_operators}: {name}, {datetime.now()}")
-            # operator = PrtsScrapperCharacter(name, name_cn, url)
-            fail_dict[name] = operator_pages[name]
+            print(f"RETRY {counter} / {num_operators}: {name}, {datetime.now()}")
 
             # Then retry to scrape operator
             operator = PrtsScrapperCharacter(name, name_cn, url)
             translated_name = operator.name_translated
-            operators_dict[translated_name] = operator.operator_details
+            operators_list.append(operator.operator_details)
             
 
         counter += 1
+
         time.sleep(randint(3, 10))
 
 
-    with open('operators.json', 'w') as f:
-        json.dump(operators_dict, f, indent=4, ensure_ascii=False)
-
-    with open('fails.json', 'w') as f:
-        json.dump(fail_dict, f, indent=4, ensure_ascii=False)
+    with open(os.path.join(out_path, "dataset.json"), "w") as f:
+        json.dump(operators_list, f, indent=4, ensure_ascii=False)
