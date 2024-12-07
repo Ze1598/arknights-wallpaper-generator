91cf84cfd7e4cef26ad29fb5953beca8386d77b4
Ze1598 <jose.fernando.costa.1998@gmail.com>
2024-12-07 17:57:28 +0000

    Loop failed scrapes until they succeed

--- static/prts_scrape.py
+++ static/prts_scrape.py
@@ -24,6 +24,9 @@ if __name__ == "__main__":
     # Keep track of operator details in a list of dictionaries
     operators_list = list()
 
+    # List to keep track of failed scrapes
+    failed_list = list()
+
     counter = 1
 
     for name in operator_pages:
@@ -32,35 +35,46 @@ if __name__ == "__main__":
         print(f"{counter} / {num_operators}: {name}, {datetime.now()}")
 
         try:
-            # if nameoperators_dict[translated_name] == "Castle-3":
-            # if name == "Vina Victoria":
-            # if name in ("Vina Victoria", "Degenbrecher", "SilverAsh", "Castle-3"):
-            # if name in ("Degenbrecher", "SilverAsh"):
+            # Scrape operator
             operator = PrtsScrapperCharacter(name, name_cn, url)
-            # print(operator.skins)
-            # print(operator.page_url)
-            # print()
-            
             # Use translated name to account for russian characters
             translated_name = operator.name_translated
-
             # Entry for this operator
             operators_list.append(operator.operator_details)
 
-        # Use the except to retry scrape
+        # Use the except to record failed scrape
         except:
+            print(f"FAILED {name}, {datetime.now()}")
+            failed_list.append(name)
             time.sleep(60)
-            print(f"RETRY {counter} / {num_operators}: {name}, {datetime.now()}")
-
-            # Then retry to scrape operator
-            operator = PrtsScrapperCharacter(name, name_cn, url)
-            translated_name = operator.name_translated
-            operators_list.append(operator.operator_details)
-            
 
         counter += 1
 
         time.sleep(randint(3, 10))
+
+    # Loop while there are no failed operators
+    print()
+    while len(failed_list) > 0:
+        print(f"{len(failed_list)} fails remain, {datetime.now()}")
+        # Always take the first operator from the list
+        name = failed_list[0]
+        name_cn = operator_pages[name]["name_cn"]
+        url = operator_pages[name]["url"]
+
+        try:
+            print(f"REPEAT {name} from {len(failed_list)} fails, {datetime.now()}")
+            operator = PrtsScrapperCharacter(name, name_cn, url)
+            translated_name = operator.name_translated
+            operators_list.append(operator.operator_details)
+        except:
+            print(f"REPEAT FAIL {name}, {datetime.now()}")
+            continue
+        
+        # If scrape successful, then delete it from the list
+        del failed_list[0]
+
+        time.sleep(60)
+
 
 
     with open(os.path.join(out_path, "dataset.json"), "w") as f:
